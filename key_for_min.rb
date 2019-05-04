6a152ee6349fc91df09f62305c77cfe821c98329
Kevin Dai <daikevin19@yahoo.com>
2019-05-04 06:19:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,14 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else 
-    min_key = name_hash.first[0]
-    min_value = name_hash.first[1]
+    min_key = name_hash.first[0] ##first[0] of a hash is it's key
+    min_value = name_hash.first[1] ##first[1] of a hash is it's value
     name_hash.each do |key, value|
-      if value < min_value
-        min_value = value
+      if value < min_value ##if new value is more less
+        min_value = value ##replaces value 
         min_key = key
       end
     end
-    return min_key
+    return min_key ##last return value in 'else'
   end
 end
